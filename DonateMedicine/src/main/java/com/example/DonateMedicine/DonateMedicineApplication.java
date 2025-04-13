05d013e8e149f6591436f6a15170b20e0bc72078
IvysonSiqueira <161773865+IvysonSiqueira@users.noreply.github.com>
2025-04-13 07:22:45 -0300

    Add files via upload

---
+++ DonateMedicine/src/main/java/com/example/DonateMedicine/DonateMedicineApplication.java
@@ -0,0 +1,13 @@
+package com.example.DonateMedicine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonateMedicineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DonateMedicineApplication.class, args);
+	}
+
+}
---
+++ DonateMedicine/src/test/java/com/example/DonateMedicine/DonateMedicineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DonateMedicine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonateMedicineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
